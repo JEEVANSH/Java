4dc13cc9606c171a49860bc5a9e44c8878b84d99
JEEVANSH <60225293+JEEVANSH@users.noreply.github.com>
2022-05-05 17:11:21 +0530

    Add files via upload

---
+++ ConcurrentCollecction1.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentCollecction1 {
+public static void main(String[] args) {
+		
+		ConcurrentHashMap<Integer,String> map = new ConcurrentHashMap<Integer,String>();
+		map.put(0, "Basics");
+		map.put(1, "Strong");
+		map.put(0,"Tech");
+		System.out.println(map);
+		//map.remove(1);
+		
+		
+		map.putIfAbsent(0,"Hello");
+		System.out.println(map);
+		
+		
+	
+		map.remove(1,"Strong");
+		System.out.println(map);
+		
+		map.replace(0, "Wrong", "Know");
+		System.out.println(map);
+		
+		map.replace(0,"BasicsStrong");
+		System.out.println(map);
+		System.out.println(map.replace(3,"BasicsStrong"));
+
+	}
+
+}
