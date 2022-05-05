3b0db187a11b3ad26f112329d30dd5a8ce129451
JEEVANSH <60225293+JEEVANSH@users.noreply.github.com>
2022-05-05 17:06:48 +0530

    Add files via upload

---
+++ TreeMap1.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class TreeMap1 {
+	public static void main(String[] args) {
+		Map<Integer,String> indmap = new TreeMap<Integer, String>();
+		indmap.put(171, "Dohni");
+		indmap.put(82, "Virat");
+		indmap.put(10, "Rohit");
+		indmap.put(12, "Sachin");
+		indmap.put(19, "Raina");
+		
+		
+		System.out.println(indmap);
+		}
+		
+}
